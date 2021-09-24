9487e3133a24ae3343351db5413bf4380c9b7905
jjjadey <nattakan.premp@gmail.com>
2021-09-24 18:35:36 +0700

    Storing User's Details

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,33 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import Login from './login';
 import Loading from '../components/Loading';
+import { useEffect } from 'react';
+// import { collection, getDocs } from 'firebase/firestore/lite';
+import { collection, doc, setDoc, serverTimestamp } from "firebase/firestore";
+
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      const docRef = doc(db, "users", user.uid);
+      const payload = {
+        email: user.email,
+        lastSeen: serverTimestamp(),
+        photoURL: user.photoURL,
+      }
+      console.log(payload);
+      setDoc(docRef, payload, { merge: true });
+      // collection("users").doc(String(user.uid) ).set({
+      //   email: user.email,
+      //   lastSeen: firebase.firestore.FieldValue.serverTimastamp(),
+      //   photoURL: user.photoURL,
+      // },
+      //   { merge: true }
+      // );
+    }
+  }, [user])
 
   if (loading) return <Loading />
   if (!user) return <Login />;
