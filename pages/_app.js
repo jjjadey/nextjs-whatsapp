932ced31796740ac2e4365b9086452f37fab12c3
jjjadey <nattakan.premp@gmail.com>
2021-09-24 14:46:26 +0700

    Login screen and GoogleAuth signin

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
+import Login from './login';
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
-  // if (!user) return <Login />;
+  // if (loading) return <Loading/>
+  if (!user) return <Login />;
   return <Component {...pageProps} />
 }
 
