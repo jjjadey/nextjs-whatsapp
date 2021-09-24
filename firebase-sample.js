ac0ada148b2e6eee07b2d88cd5123c7111877b45
jjjadey <nattakan.premp@gmail.com>
2021-09-24 13:18:32 +0700

    Setting firebase and .gitignore

---
+++ firebase-sample.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider };
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebase';
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+
+  // if (!user) return <Login />;
   return <Component {...pageProps} />
 }
 
